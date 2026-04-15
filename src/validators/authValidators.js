7281edeec9595d4a9352575988945ff1a16c013c
Parsa Ahmadi Nasab Emran <pane81042@gmail.com>
2026-04-15 12:24:49 -0400

    Add request validators for auth routes

---
+++ src/validators/authValidators.js
@@ -0,0 +1,75 @@
+import { body } from "express-validator";
+
+export const registerValidator = [
+    body('username')
+        .trim()
+        .notEmpty()
+        .withMessage('Username is required')
+        .isLength({ min: 3, max: 30 })
+        .withMessage('Username must be between 3 and 30 characters'),
+
+    body('email')
+        .trim()
+        .notEmpty()
+        .withMessage('Email is required')
+        .isEmail()
+        .withMessage('Please provide a valid email address')
+        .normalizeEmail(),
+
+    body('password')
+        .notEmpty()
+        .withMessage('Password is required')
+        .isLength({ min: 8 })
+        .withMessage('Password must be at least 8 characters long')
+];
+
+export const loginValidator = [
+    body('email')
+        .trim()
+        .notEmpty()
+        .withMessage('Email is required')
+        .isEmail()
+        .withMessage('Please provide a valid email address')
+        .normalizeEmail(),
+
+    body('password')
+        .notEmpty()
+        .withMessage('Password is required')
+];
+
+export const forgotPasswordValidator = [
+    body('email')
+        .trim()
+        .notEmpty()
+        .withMessage('Email is required')
+        .isEmail()
+        .withMessage('Please provide a valid email address')
+        .normalizeEmail()
+];
+
+export const resetPasswordValidator = [
+    body('token')
+        .trim()
+        .notEmpty()
+        .withMessage('Reset token is required'),
+
+    body('password')
+        .notEmpty()
+        .withMessage('Password is required')
+        .isLength({ min: 8 })
+        .withMessage('Password must be at least 8 characters long')
+];
+
+export const verifyEmailValidator = [
+    body('token')
+        .trim()
+        .notEmpty()
+        .withMessage('Verification token is required')
+];
+
+export const refreshTokenValidator = [
+    body('refreshToken')
+        .optional()
+        .isString()
+        .withMessage('Refresh token must be a string')
+];
